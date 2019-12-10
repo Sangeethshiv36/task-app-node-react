ead6608596123ba645278938d4827de5fe948fc6
SangeethShiv36 <b.sangeethsivan@gmail.com>
2019-12-10 12:25:48 +0530

    added client and added styles

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div className='container'>
+      <div className='jumbotron mt-5'>
+        <div className='col-sm-8 mx-auto'>
+          <h1 className='text-center'>WELCOME</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,73 @@
 const db = require('../db.json');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-  res.json(db);
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ erros: errors.array() });
+    }
 
-router.post('/', (req, res) => {
-  res.send('Login user');
+    const { email, password } = req.body;
+
+    try {
+      let user = await db.find(data => data.email === email);
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      const pwdMatch = await bcrypt.compare(password, user.password);
+
+      if (!pwdMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 50000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await db.find(data => data.id === req.user.id);
+    if (user) {
+      delete user.password;
+    }
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
